b478c91c6fac4e55e99214f7f00a92eef1544c3b
feryzb <feruzbscit@gmail.com>
2023-07-24 13:29:19 +0500

    exam

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const loading = document.getElementById("loading");
+
+window.addEventListener("load", () => {
+  loading.classList.add('loading-none');
+});
+
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add('backtop-show')
+  } else {
+    backtop.classList.remove('backtop-show')
+  }
+}
